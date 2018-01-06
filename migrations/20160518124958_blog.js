453c6e55abf7d3bd7b21777b0cec114433c86cbe
github username <github email address>
2018-01-06 08:28:21 -0600

    Done.

--- migrations/20160518124958_blog.js
+++ migrations/20160518124958_blog.js
@@ -1,8 +1,32 @@
-
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTableIfNotExists('users', (table) => {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('username');
+      table.string('email');
+      table.timestamps();
+    }),
+    knex.schema.createTableIfNotExists('posts', (table) => {
+      table.increments().primary();
+      table.string('title');
+      table.string('body');
+      table.integer('author').references('users.id');
+      table.timestamps();
+    }),
+    knex.schema.createTableIfNotExists('comments', (table) => {
+      table.increments().primary();
+      table.string('body');
+      table.integer('user_id').references('users.id');
+      table.integer('post_id').references('posts.id');
+      table.timestamps();
+    })
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema
+    .dropTable('comments')
+    .dropTable('posts')
+    .dropTable('users');
 };
