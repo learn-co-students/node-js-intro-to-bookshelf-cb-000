d768bb51efd5bc8d2678a49acb9e0da704a32749
tahbristol <tahbristol@gmail.com>
2018-01-21 21:33:00 -0500

    Done.

--- blog_server.js
+++ blog_server.js
@@ -15,6 +15,199 @@ const knex = require('knex')(config[process.env.NODE_ENV]);
 const bookshelf = require('bookshelf')(knex);
 
 // This is a good place to start!
+const User = bookshelf.Model.extend({
+	tableName: 'users',
+	hasTimestamps: true,
+	posts: function() {
+		return this.hasMany(Posts, 'author'); //by default would have been post_id
+																					//but since that is an author in the post model
+																					//we specify the author attribute.
+	},
+	comments: function() {
+		return this.hasMany(Comments);
+	}
+})
+
+const Posts = bookshelf.Model.extend({
+	tableName: 'posts',
+	hasTimestamps: true,
+	author: function() {
+		return this.belongsTo(User, 'author');
+	},
+	comments: function() {
+		return this.hasMany(Comments);
+	}
+})
+
+const Comments = bookshelf.Model.extend({
+	tableName: 'comments',
+	hasTimestamps: true,
+	user: function() {
+		return this.belongsTo(User);
+	},
+	post: function() {
+		return this.belongsTo(Posts);
+	}
+})
+exports.User = User;
+exports.Posts = Posts;
+exports.Comments = Comments;
+
+
+
+
+app.get('/', (req,res) => {
+	res.send("Hello, world.");
+});
+
+app.post('/user', (req, res) => {
+	 let remail = req.body.email;
+	 let rname = req.body.name;
+	 let rusername = req.body.username;
+
+	 if(remail && rname && rusername){
+		 knex('users')	//need to change this stuff to Bookshelf.js queries!!
+		 .returning('id')
+		 .insert({
+			 name: rname,
+			 email: remail,
+			 username: rusername
+		 })
+		 .then((user) => {
+			 res.send({id: parseInt(user)});
+		 })
+	 }else {
+		 res.status(400).send();
+	 }
+});
+
+app.get('/user/:id', (req, res) => {
+	knex.select('id','name','username','email','created_at', 'updated_at')
+	.from('users')
+	.where({id: req.params.id})
+	.then((user) => {
+		if (user.length){
+			res.send(user[0]);
+		}else {
+			res.status(404).send();
+		}
+	})
+});
+
+app.post('/post', (req, res) => {
+	let pTitle = req.body.title;
+	let pBody = req.body.body;
+	let pAuthor = req.body.author;
+
+
+	if(pTitle && pBody && pAuthor){
+
+		knex('posts')
+		.returning('id')
+		.insert({
+			title: pTitle,
+			body: pBody,
+			author: pAuthor
+		})
+		.then((post) => {
+			res.send({id: parseInt(post)});
+		})
+	}else {
+		res.status(400).send();
+	}
+})
+
+app.get('/post/:id', (req, res) => {
+
+
+	Posts.where({id: req.params.id})
+		.fetch({withRelated: ['author', 'comments']})
+		.then((post) => {
+			 if (post){
+				 res.send(post);
+
+			 }else {
+				 res.status(404).send();
+				}
+		})
+});
+
+
+app.get('/posts', (req,res) => {
+	Posts.fetchAll()
+	.then((posts) => {
+		res.send(posts);
+	})
+});
+
+app.post('/comment', (req,res) => {
+	console.log(req.body)
+	const user_id = req.body.user_id;
+	const post_id = req.body.post_id;
+	const cBody = req.body.body;
+	let cUser = {};
+	let cPost = {};
+  if(user_id && post_id && cBody){
+
+		  knex.select('id','name','username','email', 'created_at', 'updated_at')
+			.from('users')
+			.where({id: user_id})
+			.then((user) => {
+				cUser = user;
+			});
+
+			knex.select('id', 'title', 'author', 'body', 'created_at', 'updated_at')
+			.from('posts')
+			.where({id: post_id})
+			.then((post) => {
+				cPost = post;
+			})
+
+			knex('comments')	//need to change this stuff to Bookshelf.js queries!!
+			.returning('id')
+			.insert({
+				body: cBody,
+				user_id: cUser.id,
+				post_id: cPost.id
+			})
+			.then((comment) => {
+				res.send({id:parseInt(comment)})
+			})
+
+	}else {
+		res.status(400).send();
+	}
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -41,4 +234,3 @@ exports.up = (justBackend) => {
       console.log('Listening on port 3000...');
     });
 };
-
--- migrations/20160518124958_blog.js
+++ migrations/20160518124958_blog.js
@@ -1,8 +1,35 @@
 
 exports.up = function(knex, Promise) {
-  
+	return Promise.all([
+		knex.schema.createTable('users', (tbl) => {
+				tbl.increments('id').primary();
+				tbl.string('name');
+				tbl.string('email');
+				tbl.string('username');
+				tbl.timestamps();
+			}),
+
+		knex.schema.createTable('posts', (tbl) => {
+			tbl.increments().primary();
+			tbl.string('title');
+			tbl.string('body');
+			tbl.integer('author').references('users.id');
+			tbl.timestamps();
+		}),
+
+		knex.schema.createTable('comments', (tbl) => {
+			tbl.increments().primary();
+			tbl.integer('user_id').references('users.id');
+			tbl.integer('post_id').references('posts.id');
+			tbl.string('body');
+			tbl.timestamps();
+		})
+	]);
 };
 
 exports.down = function(knex, Promise) {
-  
-};
+	return knex.schema
+	    .dropTable('comments')
+	    .dropTable('posts')
+	    .dropTable('users');
+		};
